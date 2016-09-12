c204d26704ad119ada48388a936e0b9f65b0de79
matt <matt@DUNWEEZY>
2016-09-12 07:52:23 -0500

    Add project files.

---
+++ TDSPaster/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TDSPaster
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.PasteDataButton = new System.Windows.Forms.Button();
+            this.SaveFileButton = new System.Windows.Forms.Button();
+            this.SelectFileButton = new System.Windows.Forms.Button();
+            this.previewListBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 230);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(870, 263);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // PasteDataButton
+            // 
+            this.PasteDataButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasteDataButton.Location = new System.Drawing.Point(286, 46);
+            this.PasteDataButton.Name = "PasteDataButton";
+            this.PasteDataButton.Size = new System.Drawing.Size(333, 163);
+            this.PasteDataButton.TabIndex = 1;
+            this.PasteDataButton.Text = "Paste Data";
+            this.PasteDataButton.UseVisualStyleBackColor = true;
+            this.PasteDataButton.Click += new System.EventHandler(this.PasteDataButton_Click);
+            // 
+            // SaveFileButton
+            // 
+            this.SaveFileButton.Location = new System.Drawing.Point(651, 99);
+            this.SaveFileButton.Name = "SaveFileButton";
+            this.SaveFileButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveFileButton.TabIndex = 2;
+            this.SaveFileButton.Text = "Save File";
+            this.SaveFileButton.UseVisualStyleBackColor = true;
+            this.SaveFileButton.Click += new System.EventHandler(this.SaveFileButton_Click);
+            // 
+            // SelectFileButton
+            // 
+            this.SelectFileButton.Location = new System.Drawing.Point(651, 46);
+            this.SelectFileButton.Name = "SelectFileButton";
+            this.SelectFileButton.Size = new System.Drawing.Size(75, 23);
+            this.SelectFileButton.TabIndex = 3;
+            this.SelectFileButton.Text = "Select File";
+            this.SelectFileButton.UseVisualStyleBackColor = true;
+            this.SelectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // previewListBox
+            // 
+            this.previewListBox.FormattingEnabled = true;
+            this.previewListBox.Location = new System.Drawing.Point(13, 13);
+            this.previewListBox.Name = "previewListBox";
+            this.previewListBox.Size = new System.Drawing.Size(267, 199);
+            this.previewListBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 505);
+            this.Controls.Add(this.previewListBox);
+            this.Controls.Add(this.SelectFileButton);
+            this.Controls.Add(this.SaveFileButton);
+            this.Controls.Add(this.PasteDataButton);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button PasteDataButton;
+        private System.Windows.Forms.Button SaveFileButton;
+        private System.Windows.Forms.Button SelectFileButton;
+        private System.Windows.Forms.ListBox previewListBox;
+    }
+}
+
---
+++ TDSPaster/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TDSPaster
+{
+    public partial class Form1 : Form
+    {
+        string fileLocation;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PasteDataButton_Click(object sender, EventArgs e)
+        {//paste data from clipboard
+            DataObject o = (DataObject)Clipboard.GetDataObject();
+            if (o.GetDataPresent(DataFormats.Text))
+            {
+                if (dataGridView1.RowCount > 0)
+                    dataGridView1.Rows.Clear();
+
+                if (dataGridView1.ColumnCount > 0)
+                    dataGridView1.Columns.Clear();
+
+                bool columnsAdded = false;
+                string[] pastedRows = Regex.Split(o.GetData(DataFormats.Text).ToString().TrimEnd("\r\n".ToCharArray()), "\r\n");
+                int j = 0;
+                foreach (string pastedRow in pastedRows)
+                {
+                    string[] pastedRowCells = pastedRow.Split(new char[] { '\t' });
+
+                    if (!columnsAdded)
+                    {
+                        for (int i = 0; i < pastedRowCells.Length; i++)
+                            dataGridView1.Columns.Add("col" + i, pastedRowCells[i]);
+
+                        columnsAdded = true;
+                        continue;
+                    }
+
+                    dataGridView1.Rows.Add();
+                    int myRowIndex = dataGridView1.Rows.Count - 1;
+
+                    using (DataGridViewRow dataGridView1Row = dataGridView1.Rows[j])
+                    {
+                        for (int i = 0; i < pastedRowCells.Length; i++)
+                            dataGridView1Row.Cells[i].Value = pastedRowCells[i];
+                    }
+                    j++;
+                }
+            }
+        }
+
+        private void SelectFileButton_Click(object sender, EventArgs e)
+        {
+            // Create an instance of the open file dialog box.
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set filter options and filter index.
+            openFileDialog1.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+
+            openFileDialog1.Multiselect = true;
+
+            // Call the ShowDialog method to show the dialog box.
+            DialogResult userClickedOK = openFileDialog1.ShowDialog();
+
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == DialogResult.OK)
+            {
+                // Open the selected file to read.
+                System.IO.Stream fileStream = openFileDialog1.OpenFile();
+
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                {
+                    // Read the first line from the file and write it the textbox.
+                    //previewListBox.Items.Add(reader.ReadLine());
+
+
+                    string filename = openFileDialog1.FileName;
+
+                    fileLocation = filename;
+
+                    //textBox2.Text = filename;
+
+                    //folderLocation = Path.GetDirectoryName(filename);
+                }
+                fileStream.Close();
+                // Code that is going to fetch the tube count and other goodies!
+                string line;
+                int counter = 0;
+
+                StreamReader reader2 = new StreamReader(fileLocation);
+
+
+                while ((line = reader2.ReadLine()) != null)
+                {
+                    previewListBox.Items.Add(line);
+                    counter++;
+                }
+                float finalTubeCount = ((counter - 18f) / 3f);
+                Global.GlobalVar = finalTubeCount;
+                Debug.WriteLine("Tube count:" + finalTubeCount);
+                reader2.Close();
+            }
+        }
+        //creating the class that will allow for global variable use.
+        //this is for passing the tube count to the conversion routine
+        static class Global
+        {
+            private static float _globalVar;
+            private static int _globalCounter;
+
+            public static float GlobalVar
+            {
+                get { return _globalVar; }
+                set { _globalVar = value; }
+
+            }
+            public static int GlobalCounter
+            {
+                get { return _globalCounter; }
+                set { _globalCounter = value; }
+            }
+        }
+        
+        private void SaveFileButton_Click(object sender, EventArgs e)
+        { 
+            StreamWriter sW = new StreamWriter(fileLocation);
+
+            //string lines = "";
+            //string data = "";
+
+            /*for(int row = 2; row <=4; row++ )
+            {
+                for (int col = 2; col <= (int)Global.GlobalVar; col++)
+            {
+
+                    data += (string.IsNullOrEmpty(data) ? " ": ", ") + dataGridView1.Rows[row].Cells[col].Value.ToString();
+                    sW.WriteLine(dataGridView1.Rows[row].Cells[col].Value.ToString());
+
+                }
+                sW.WriteLine("will this even fucking work?");
+            }*/
+
+
+        sW.WriteLine("will this even fucking work?");
+            sW.WriteLine(dataGridView1.Rows[3].Cells[3].Value.ToString());
+
+            sW.Close();
+        }
+    }
+}
+
