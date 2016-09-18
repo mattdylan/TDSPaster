97e57535096c6aaed252710501311348919ac931
matt <matt@DUNWEEZY>
2016-09-17 20:34:01 -0500

    You must first select a file to write to, then paste your data from excel. Currently the program requires that left,center, and right readings have their own rows. The first row is reserved and must not contain any data you want to transfer. All left readings must be on row 2, center on row 3, right on 4. A messagebox will show if the tube count for the section does not match the tube count for the data you pasted.

--- TDSPaster/Form1.cs
+++ TDSPaster/Form1.cs
@@ -14,15 +14,38 @@ using System.Windows.Forms;
 namespace TDSPaster
 {
     public partial class Form1 : Form
-    {
+    {       
         string fileLocation;
         public Form1()
         {
             InitializeComponent();
+            PasteDataButton.Enabled = false;
+        }
+        //cleaning up the many types of comments that are not TDS compatable
+        public string dataValidator(string readingValue)
+        {
+            char[] charsToTrim = { '.', 'V' };
+            string goodData;
+            string quote = "\"";
+            string spaceQuote = " \"";
+            if (Regex.IsMatch(readingValue, @"\d"))
+            {
+                readingValue = readingValue.Trim(charsToTrim);
+                readingValue = quote + readingValue + spaceQuote;
+                return readingValue;
+                
+            }
+            else
+            {
+                goodData = @"""    """;
+                return goodData;
+            }
+
         }
 
         private void PasteDataButton_Click(object sender, EventArgs e)
-        {//paste data from clipboard
+        {
+            //paste data from clipboard
             DataObject o = (DataObject)Clipboard.GetDataObject();
             if (o.GetDataPresent(DataFormats.Text))
             {
@@ -59,10 +82,15 @@ namespace TDSPaster
                     j++;
                 }
             }
+            if (Global.GlobalVar != dataGridView1.ColumnCount)
+            {
+                MessageBox.Show("Oh no! It seems that you have pasted an amount of tubes that does not match the tube count for the section! If you continue the program will crash!");
+            }
         }
 
         private void SelectFileButton_Click(object sender, EventArgs e)
         {
+            PasteDataButton.Enabled = true;
             // Create an instance of the open file dialog box.
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
 
@@ -80,21 +108,13 @@ namespace TDSPaster
             if (userClickedOK == DialogResult.OK)
             {
                 // Open the selected file to read.
-                System.IO.Stream fileStream = openFileDialog1.OpenFile();
+                Stream fileStream = openFileDialog1.OpenFile();
 
-                using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+                using (StreamReader reader = new StreamReader(fileStream))
                 {
-                    // Read the first line from the file and write it the textbox.
-                    //previewListBox.Items.Add(reader.ReadLine());
-
-
                     string filename = openFileDialog1.FileName;
 
-                    fileLocation = filename;
-
-                    //textBox2.Text = filename;
-
-                    //folderLocation = Path.GetDirectoryName(filename);
+                    fileLocation = filename;                  
                 }
                 fileStream.Close();
                 // Code that is going to fetch the tube count and other goodies!
@@ -115,6 +135,7 @@ namespace TDSPaster
                 reader2.Close();
             }
         }
+
         //creating the class that will allow for global variable use.
         //this is for passing the tube count to the conversion routine
         static class Global
@@ -136,29 +157,65 @@ namespace TDSPaster
         }
         
         private void SaveFileButton_Click(object sender, EventArgs e)
-        { 
-            StreamWriter sW = new StreamWriter(fileLocation);
-
-            //string lines = "";
-            //string data = "";
-
-            /*for(int row = 2; row <=4; row++ )
-            {
-                for (int col = 2; col <= (int)Global.GlobalVar; col++)
-            {
-
-                    data += (string.IsNullOrEmpty(data) ? " ": ", ") + dataGridView1.Rows[row].Cells[col].Value.ToString();
-                    sW.WriteLine(dataGridView1.Rows[row].Cells[col].Value.ToString());
-
+        {
+            //creating the array, this is dynamic, the array will match the number of columns and rows
+            string[,] readingValueArray = new string[dataGridView1.Rows.Count, dataGridView1.Columns.Count];
+            //populating the array
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {                
+                foreach (DataGridViewColumn col in dataGridView1.Columns)
+                {//crawling the datagridview and storing values in the array
+                    readingValueArray[row.Index, col.Index] = dataGridView1.Rows[row.Index].Cells[col.Index].Value.ToString();
                 }
-                sW.WriteLine("will this even fucking work?");
-            }*/
+            }
+            //this is going to fetch the amount of lines in the document we are writing to
+            string[] lines = File.ReadAllLines(fileLocation);
+            //simple counters for each location on the tube
+            int leftCounter = 0;
+            int centerCounter = 0;
+            int rightCounter = 0;
 
+            
+            //outputting the array values to the TDS file
+            using (StreamWriter writer = new StreamWriter(fileLocation))
+            {
+                for (int currentLine = 1; currentLine <= lines.Length; ++currentLine)
+                {    //printing left readings to file first             
+                     if (currentLine >= 6 && currentLine <= (Global.GlobalVar +5))
+                     {
+                        string value = readingValueArray[0, leftCounter];
+                        //passing the cleanup method the value of the cell in the array, it will return the TDS acceptable value
+                        string returnedValue = dataValidator(value);
+                        
+                        writer.WriteLine(returnedValue);
+                        leftCounter++;
+                     }//printing center readings
+                    else if (currentLine > (Global.GlobalVar + 11) && currentLine <= ((Global.GlobalVar * 2) + 11))
+                    {
+                        string value = readingValueArray[1, centerCounter];
+                        //passing the cleanup method the value of the cell in the array, it will return the TDS acceptable value
+                        string returnedValue = dataValidator(value);
 
-        sW.WriteLine("will this even fucking work?");
-            sW.WriteLine(dataGridView1.Rows[3].Cells[3].Value.ToString());
+                        writer.WriteLine(returnedValue);
+                        centerCounter++;
+                    }//printing right readings
+                    else if (currentLine > ((Global.GlobalVar * 2) + 17) && currentLine <= ((Global.GlobalVar * 3) + 17))
+                     {
+                        string value = readingValueArray[2, rightCounter];
+                        //passing the cleanup method the value of the cell in the array, it will return the TDS acceptable value
+                        string returnedValue = dataValidator(value);
 
-            sW.Close();
+                        writer.WriteLine(returnedValue);
+                            rightCounter++;
+                     }
+                        
+                    else 
+                    {//not 100% sure I think this closes the stream once end of file is reached and saves it also
+                        writer.WriteLine(lines[currentLine - 1]);
+                    }                    
+                }
+            }
+            MessageBox.Show("If nothing caught fire the file was transfered!");
         }
     }
 }
