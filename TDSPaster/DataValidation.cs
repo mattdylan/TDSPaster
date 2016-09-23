379ab851074bbce170a07d7d2976478561a9d0e3
Tyler Chambers <tylerc515@gmail.com>
2016-09-23 14:15:55 -0500

    Fixes etc - Took your logic from the if statements to check reading length and added them to a switch. I also added logic for readings with 4 chars, since this will encapsulate verified readings as well. - Added a check to see if the reading is between 0 and 4 chars. If not, the reading is replaced with a blank reading. - Added ClearPastedData() method to the beginning of the pasteButton_Click method so that if the user clicks paste data multiple times, the program no longer continuously pastes data. It now clears any old pasted data first then pastes the new data. - Moved the data validation block into its own class. I think it makes the code a little nicer. - added ClearPastedData() to the ok click method of savefilebutton so that if the user selects a file, pastes data, and then chooses to select a different file, the pasted data is cleared to avoid bugs with data still being present. - added a more meaningful success message for when data is succussfully written to the file. - added a check to make sure that the file name without the extension was 6 letters, further ensuring that the file is a TDS file. This logic could be applied further when going through files (all files in a folder) to only grab the TDS files. - moved everything under the paste data button to its own method. Used an override of the keypress event to listen for ctrl+v. Added check to make sure pasting was allowed (pasteButton was enabled).

---
+++ TDSPaster/DataValidation.cs
@@ -0,0 +1,98 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TDSPaster
+{
+    class DataValidation
+    {
+        //cleaning up the many types of comments that are not TDS compatable
+        public static string DataValidator(string readingValue)
+        {
+            //check to make sure data already contains decimals. If not, continue on with validation. If so, checks and fixes decimal placement (ie data truncated from BEAM).
+            if (readingValue.Contains("."))
+            {
+                //This code checks to see if the reading has been truncated and repairs if it has
+                int count = readingValue.Length;
+                decimal floatHolder;
+                //executes if the reading has had just one zero truncated and checks for leading zero beam exports
+                if (readingValue.StartsWith("0") && count == 4)
+                {
+                    //adds a zero to the end of the reading by multiplying by the decimal
+                    floatHolder = decimal.Parse(readingValue);
+                    floatHolder = floatHolder * 1.0m;
+                    readingValue = floatHolder.ToString();
+                }
+                //executes if the reading has had two zeroes truncated and checks for leading zero beam exports
+                if (readingValue.StartsWith("0") && count == 3)
+                {
+                    floatHolder = decimal.Parse(readingValue);
+                    floatHolder = floatHolder * 1.00m;
+                    readingValue = floatHolder.ToString();
+                }
+            }
+
+            char[] charsToTrim = { '.' };//Place any characters that should not go into the TDS file here
+
+            //create an array of TDS comments 
+            var tdsComments = "+()/=<>;$[#*@?".ToCharArray();
+            //checks if reading value contains comments
+            var hasTdsComments = readingValue.IndexOfAny(tdsComments) != -1;
+
+            //the following strings encase the numeric value
+            string quote = "\"";
+            string spaceQuote = " \"";
+            string commentQuote = "\"   ";
+            string singleZero = "0";
+            string doubleZero = "00";
+            int readingLength = readingValue.Length;
+            //check to make sure the reading is between 0 and 4 chars; if not, a blank reading will be entered.
+            bool isValidLength = readingLength <= 4 && readingLength > -1;
+
+            //if the data is numbers with a single character the if statement will execute.           
+            if (Regex.IsMatch(readingValue, @"\d") && isValidLength)
+            {
+                //todo put the trimming functions in their own method
+                readingValue = readingValue.TrimStart('0'); //getting rid of any leading zeros
+                readingValue = readingValue.Trim(charsToTrim);
+
+                switch (readingLength)
+                {
+                    case 4:
+                        readingValue = quote + readingValue + quote;
+                        break;
+                    case 3:
+                        readingValue = quote + readingValue + spaceQuote;
+                        break;
+                    case 2:
+                        readingValue = quote + singleZero + readingValue + spaceQuote;
+                        break;
+                    case 1:
+                        readingValue = quote + doubleZero + readingValue + spaceQuote;
+                        break;
+                }
+            }
+            //check if the reading contains TDS comments.
+            else if (hasTdsComments)
+            {
+                //remove all whitespace and format the comment for the TDS
+                readingValue = readingValue.Replace(" ", string.Empty);
+                readingValue = commentQuote + readingValue + quote;
+            }
+
+            //if the reading is not a valid reading or TDS comment replace reading with a blank reading
+            else
+            {
+                readingValue = @"""    """;
+            }
+            return readingValue;
+        }
+
+       public static string RemoveNonAlpha(string line)
+        {
+            //Replace all multiple spaces with just one space, then remove the quotes.
+            var newLine = Regex.Replace(line, @"\s+", " ");
+            newLine = newLine.Replace("\"", string.Empty);
+            return newLine;
+        }
+    }
+}
--- TDSPaster/mainForm.Designer.cs
+++ TDSPaster/mainForm.Designer.cs
@@ -48,7 +48,9 @@
             this.colLeftLabel = new System.Windows.Forms.Label();
             this.clearAllButton = new System.Windows.Forms.Button();
             this.elevationInfoListBox = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.clearPastedButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.superSecretCheckbox = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -57,7 +59,7 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(64, 356);
+            this.dataGridView1.Location = new System.Drawing.Point(87, 373);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(734, 263);
@@ -66,7 +68,7 @@
             // PasteDataButton
             // 
             this.PasteDataButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PasteDataButton.Location = new System.Drawing.Point(339, 137);
+            this.PasteDataButton.Location = new System.Drawing.Point(347, 153);
             this.PasteDataButton.Name = "PasteDataButton";
             this.PasteDataButton.Size = new System.Drawing.Size(214, 121);
             this.PasteDataButton.TabIndex = 1;
@@ -76,7 +78,7 @@
             // 
             // SaveFileButton
             // 
-            this.SaveFileButton.Location = new System.Drawing.Point(625, 661);
+            this.SaveFileButton.Location = new System.Drawing.Point(648, 678);
             this.SaveFileButton.Name = "SaveFileButton";
             this.SaveFileButton.Size = new System.Drawing.Size(75, 23);
             this.SaveFileButton.TabIndex = 2;
@@ -87,7 +89,7 @@
             // SelectFileButton
             // 
             this.SelectFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SelectFileButton.Location = new System.Drawing.Point(339, 86);
+            this.SelectFileButton.Location = new System.Drawing.Point(347, 102);
             this.SelectFileButton.Name = "SelectFileButton";
             this.SelectFileButton.Size = new System.Drawing.Size(214, 34);
             this.SelectFileButton.TabIndex = 3;
@@ -98,16 +100,16 @@
             // previewListBox
             // 
             this.previewListBox.FormattingEnabled = true;
-            this.previewListBox.Location = new System.Drawing.Point(64, 85);
+            this.previewListBox.Location = new System.Drawing.Point(87, 102);
             this.previewListBox.Name = "previewListBox";
-            this.previewListBox.Size = new System.Drawing.Size(267, 173);
+            this.previewListBox.Size = new System.Drawing.Size(254, 173);
             this.previewListBox.TabIndex = 4;
             // 
             // elevationOverwriteLabel
             // 
             this.elevationOverwriteLabel.AutoSize = true;
             this.elevationOverwriteLabel.ForeColor = System.Drawing.Color.Red;
-            this.elevationOverwriteLabel.Location = new System.Drawing.Point(559, 68);
+            this.elevationOverwriteLabel.Location = new System.Drawing.Point(564, 85);
             this.elevationOverwriteLabel.Name = "elevationOverwriteLabel";
             this.elevationOverwriteLabel.Size = new System.Drawing.Size(229, 13);
             this.elevationOverwriteLabel.TabIndex = 13;
@@ -116,16 +118,16 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(63, 68);
+            this.label6.Location = new System.Drawing.Point(84, 85);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(105, 13);
+            this.label6.Size = new System.Drawing.Size(108, 13);
             this.label6.TabIndex = 14;
-            this.label6.Text = "Selected file preview";
+            this.label6.Text = "Selected file preview:";
             // 
             // rowLeftLabel
             // 
             this.rowLeftLabel.AutoSize = true;
-            this.rowLeftLabel.Location = new System.Drawing.Point(33, 362);
+            this.rowLeftLabel.Location = new System.Drawing.Point(56, 379);
             this.rowLeftLabel.Name = "rowLeftLabel";
             this.rowLeftLabel.Size = new System.Drawing.Size(25, 13);
             this.rowLeftLabel.TabIndex = 15;
@@ -134,7 +136,7 @@
             // rowCenterLabel
             // 
             this.rowCenterLabel.AutoSize = true;
-            this.rowCenterLabel.Location = new System.Drawing.Point(20, 384);
+            this.rowCenterLabel.Location = new System.Drawing.Point(43, 401);
             this.rowCenterLabel.Name = "rowCenterLabel";
             this.rowCenterLabel.Size = new System.Drawing.Size(38, 13);
             this.rowCenterLabel.TabIndex = 16;
@@ -143,7 +145,7 @@
             // rowRightLabel
             // 
             this.rowRightLabel.AutoSize = true;
-            this.rowRightLabel.Location = new System.Drawing.Point(26, 409);
+            this.rowRightLabel.Location = new System.Drawing.Point(49, 426);
             this.rowRightLabel.Name = "rowRightLabel";
             this.rowRightLabel.Size = new System.Drawing.Size(32, 13);
             this.rowRightLabel.TabIndex = 17;
@@ -152,7 +154,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(355, 279);
+            this.label10.Location = new System.Drawing.Point(363, 295);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(184, 13);
             this.label10.TabIndex = 18;
@@ -161,7 +163,7 @@
             // sublimeCheckBox
             // 
             this.sublimeCheckBox.AutoSize = true;
-            this.sublimeCheckBox.Location = new System.Drawing.Point(625, 638);
+            this.sublimeCheckBox.Location = new System.Drawing.Point(648, 655);
             this.sublimeCheckBox.Name = "sublimeCheckBox";
             this.sublimeCheckBox.Size = new System.Drawing.Size(173, 17);
             this.sublimeCheckBox.TabIndex = 19;
@@ -172,7 +174,7 @@
             // 
             this.singleRowRadioButton.AutoSize = true;
             this.singleRowRadioButton.Checked = true;
-            this.singleRowRadioButton.Location = new System.Drawing.Point(358, 299);
+            this.singleRowRadioButton.Location = new System.Drawing.Point(366, 315);
             this.singleRowRadioButton.Name = "singleRowRadioButton";
             this.singleRowRadioButton.Size = new System.Drawing.Size(79, 17);
             this.singleRowRadioButton.TabIndex = 20;
@@ -184,7 +186,7 @@
             // tripleRowRadioButton
             // 
             this.tripleRowRadioButton.AutoSize = true;
-            this.tripleRowRadioButton.Location = new System.Drawing.Point(441, 299);
+            this.tripleRowRadioButton.Location = new System.Drawing.Point(449, 315);
             this.tripleRowRadioButton.Name = "tripleRowRadioButton";
             this.tripleRowRadioButton.Size = new System.Drawing.Size(76, 17);
             this.tripleRowRadioButton.TabIndex = 21;
@@ -195,7 +197,7 @@
             // colRightLabel
             // 
             this.colRightLabel.AutoSize = true;
-            this.colRightLabel.Location = new System.Drawing.Point(329, 340);
+            this.colRightLabel.Location = new System.Drawing.Point(352, 357);
             this.colRightLabel.Name = "colRightLabel";
             this.colRightLabel.Size = new System.Drawing.Size(32, 13);
             this.colRightLabel.TabIndex = 24;
@@ -204,7 +206,7 @@
             // colCenterLabel
             // 
             this.colCenterLabel.AutoSize = true;
-            this.colCenterLabel.Location = new System.Drawing.Point(232, 340);
+            this.colCenterLabel.Location = new System.Drawing.Point(255, 357);
             this.colCenterLabel.Name = "colCenterLabel";
             this.colCenterLabel.Size = new System.Drawing.Size(38, 13);
             this.colCenterLabel.TabIndex = 23;
@@ -213,7 +215,7 @@
             // colLeftLabel
             // 
             this.colLeftLabel.AutoSize = true;
-            this.colLeftLabel.Location = new System.Drawing.Point(139, 340);
+            this.colLeftLabel.Location = new System.Drawing.Point(162, 357);
             this.colLeftLabel.Name = "colLeftLabel";
             this.colLeftLabel.Size = new System.Drawing.Size(25, 13);
             this.colLeftLabel.TabIndex = 22;
@@ -221,7 +223,7 @@
             // 
             // clearAllButton
             // 
-            this.clearAllButton.Location = new System.Drawing.Point(142, 661);
+            this.clearAllButton.Location = new System.Drawing.Point(165, 678);
             this.clearAllButton.Name = "clearAllButton";
             this.clearAllButton.Size = new System.Drawing.Size(75, 23);
             this.clearAllButton.TabIndex = 25;
@@ -232,27 +234,49 @@
             // elevationInfoListBox
             // 
             this.elevationInfoListBox.FormattingEnabled = true;
-            this.elevationInfoListBox.Location = new System.Drawing.Point(562, 84);
+            this.elevationInfoListBox.Location = new System.Drawing.Point(567, 101);
             this.elevationInfoListBox.Name = "elevationInfoListBox";
-            this.elevationInfoListBox.Size = new System.Drawing.Size(236, 173);
+            this.elevationInfoListBox.Size = new System.Drawing.Size(254, 173);
             this.elevationInfoListBox.TabIndex = 27;
             // 
-            // button1
+            // clearPastedButton
             // 
-            this.button1.Location = new System.Drawing.Point(61, 661);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 28;
-            this.button1.Text = "Clear Pasted";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.clearPastedButton.Location = new System.Drawing.Point(84, 678);
+            this.clearPastedButton.Name = "clearPastedButton";
+            this.clearPastedButton.Size = new System.Drawing.Size(75, 23);
+            this.clearPastedButton.TabIndex = 28;
+            this.clearPastedButton.Text = "Clear Pasted";
+            this.clearPastedButton.UseVisualStyleBackColor = true;
+            this.clearPastedButton.Click += new System.EventHandler(this.clearPastedButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(202, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(496, 55);
+            this.titleLabel.TabIndex = 29;
+            this.titleLabel.Text = "TDS Data Paste Tool";
+            // 
+            // superSecretCheckbox
+            // 
+            this.superSecretCheckbox.AutoSize = true;
+            this.superSecretCheckbox.Location = new System.Drawing.Point(729, 682);
+            this.superSecretCheckbox.Name = "superSecretCheckbox";
+            this.superSecretCheckbox.Size = new System.Drawing.Size(106, 17);
+            this.superSecretCheckbox.TabIndex = 30;
+            this.superSecretCheckbox.Text = "Do ya love meh?";
+            this.superSecretCheckbox.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(835, 694);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(884, 711);
+            this.Controls.Add(this.superSecretCheckbox);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.clearPastedButton);
             this.Controls.Add(this.elevationInfoListBox);
             this.Controls.Add(this.clearAllButton);
             this.Controls.Add(this.colRightLabel);
@@ -273,6 +297,9 @@
             this.Controls.Add(this.PasteDataButton);
             this.Controls.Add(this.dataGridView1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(900, 750);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "TDS Data Paste Tool";
@@ -304,7 +331,9 @@
         private System.Windows.Forms.Label colLeftLabel;
         private System.Windows.Forms.Button clearAllButton;
         private System.Windows.Forms.ListBox elevationInfoListBox;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clearPastedButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.CheckBox superSecretCheckbox;
     }
 }
 
